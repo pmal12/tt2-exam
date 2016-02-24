3f914e1b33160a4dc299f04448f1875a7f910c8f
Ray Allen <r.allen@inbox.lv>
2016-02-24 15:46:24 +0200

    Final

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -73,12 +73,12 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_order
       @order = Order.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,12 +64,12 @@ class ProductsController < ApplicationController
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
